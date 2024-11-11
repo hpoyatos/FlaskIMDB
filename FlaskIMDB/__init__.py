934a031e9475523279092f8c558d10690e94e432
Henrique Poyatos <hpoyatos@yahoo.com.br>
2024-11-11 18:41:49 -0300

    ajuste

--- FlaskIMDB/__init__.py
+++ FlaskIMDB/__init__.py
@@ -6,6 +6,17 @@ from .routes.filmeBp import filmeBp
 import os
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(PROJECT_ROOT, 'database/imdb.sqlite.db')
+#print(app.config['SQLALCHEMY_DATABASE_URI'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate.init_app(app)
+
+#e adiciona isso
+app.register_blueprint(filmeBp)
+'''
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(PROJECT_ROOT, 'database/imdb.sqlite.db')
@@ -19,3 +30,4 @@ def create_app():
     app.register_blueprint(filmeBp)
 
     return app
+'''
